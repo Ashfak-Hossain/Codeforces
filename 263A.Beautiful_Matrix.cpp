0ecd2f299d8e9f3b06297f1398ea305ad75431bd
Berlin <evan1234.ek@gmail.com>
2023-02-20 12:29:20 +0600

    solved

---
+++ 263A.Beautiful_Matrix.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Ashfak Hossain Evan, CSE, American International University-Bangladesh (AIUB)
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    // Code Start From Here
+
+    int x(0);
+
+    for (int i = 1; i <= 5; ++i)
+    {
+        for (int j = 1; j <= 5; ++j)
+        {
+            cin >> x;
+            if (x == 1)
+            {
+                cout << abs(i - 3) + abs(j - 3) << endl;
+            }
+        }
+    }
+
+    return 0;
+}
