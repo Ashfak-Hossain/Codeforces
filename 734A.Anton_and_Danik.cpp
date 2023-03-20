74fd47343c45fe83a05fba858159fea28fc39493
Berlin <evan1234.ek@gmail.com>
2023-03-20 22:54:54 +0600

    Solved

---
+++ 734A.Anton_and_Danik.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Ashfak Hossain Evan, CSE, American International University-Bangladesh (AIUB)
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    // Code Start From Here
+
+    
+
+    return 0;
+}
