ef03d440adfdfa28dc43757ad826e5748837d102
Berlin <evan1234.ek@gmail.com>
2023-03-20 23:02:16 +0600

    Solved

--- 734A.Anton_and_Danik.cpp
+++ 734A.Anton_and_Danik.cpp
@@ -12,7 +12,38 @@ int main()
 
     // Code Start From Here
 
-    
+    int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+
+    int a(0), d(0);
+
+    for (size_t i = 0; i < n; i++)
+    {
+        if (s[i] == 'A')
+        {
+            a++;
+        }
+        else
+        {
+            d++;
+        }
+    }
+
+    if (a > d)
+    {
+        cout << "Anton" << endl;
+    }
+    else if (a < d)
+    {
+        cout << "Danik" << endl;
+    }
+    else
+    {
+        cout << "Friendship" << endl;
+    }
 
     return 0;
 }
