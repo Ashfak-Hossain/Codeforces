3c57ccda266d6ab0daae2f5267e44ee62c555d27
Berlin <evan1234.ek@gmail.com>
2023-03-23 20:56:44 +0600

    Solved

---
+++ 271A.Beautiful_Year.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Ashfak Hossain Evan, CSE, American International University-Bangladesh (AIUB)
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    // Code Start From Here
+
+    int y;
+    cin >> y;
+    while (true)
+    {
+        y += 1;
+        int a = y / 1000;
+        int b = y / 100 % 10;
+        int c = y / 10 % 10;
+        int d = y % 10;
+        if (a != b && a != c && a != d && b != c && b != d && c != d)
+        {
+            break;
+        }
+    }
+    cout << y << endl;
+    return 0;
+
+    return 0;
+}
