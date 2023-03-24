28ce939fea20fa3780a0c402fab2f07b52bcdac0
Berlin <evan1234.ek@gmail.com>
2023-03-24 11:07:23 +0600

    Solved

---
+++ 344A.Magnets.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Ashfak Hossain Evan, CSE, American International University-Bangladesh (AIUB)
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    // Code Start From Here
+
+    int n, c = 0;
+    cin >> n;
+
+    int arr[100000];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] != arr[i + 1])
+            c++;
+        }
+    cout << c << endl;
+
+    return 0;
+}
