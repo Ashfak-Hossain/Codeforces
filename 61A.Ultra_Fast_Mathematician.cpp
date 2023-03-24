b8f9db08bbcada6644f120679cd6aa2a5117673c
Berlin <evan1234.ek@gmail.com>
2023-03-24 12:33:23 +0600

    Solved

---
+++ 61A.Ultra_Fast_Mathematician.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Ashfak Hossain Evan, CSE, American International University-Bangladesh (AIUB)
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    // Code Start From Here
+
+    string s1, s2;
+
+    cin >> s1 >> s2;
+
+    for (size_t i = 0; i < s1.length(); i++)
+    {
+
+        if (s1[i] == s2[i])
+        {
+            s1[i] = '0';
+        }
+        else
+        {
+            s1[i] = '1';
+        }
+    }
+    cout << s1 << endl;
+
+    return 0;
+}
