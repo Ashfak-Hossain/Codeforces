3a60be194ccbf5dcefb8248e3da3e6a6a18d8047
Berlin <evan1234.ek@gmail.com>
2023-03-27 12:38:13 +0600

    solved

---
+++ 144A.Arrival_of_the_General.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Ashfak Hossain Evan, CSE, American International University-Bangladesh (AIUB)
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int n;
+    int maxvalue = 0;
+    int minvalue = 1000;
+    int maxindex = 0;
+    int minindex = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        if (x > maxvalue)
+        {
+            maxindex = i;
+            maxvalue = x;
+        }
+        if (x <= minvalue)
+        {
+            minindex = i;
+            minvalue = x;
+        }
+    }
+    if (maxindex > minindex)
+    {
+        cout << (maxindex - 1) + (n - minindex) - 1;
+    }
+    else
+    {
+        cout << (maxindex - 1) + (n - minindex);
+    }
+
+    return 0;
+}
---
+++ A_Divisibility_Problem.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Ashfak Hossain Evan, CSE, American International University-Bangladesh (AIUB)
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    
+
+    return 0;
+}
