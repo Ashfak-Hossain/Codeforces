60554b12f21bb2f419bb0010712039a018ab0342
Berlin <evan1234.ek@gmail.com>
2023-03-27 13:00:27 +0600

    Solved

--- A_Divisibility_Problem.cpp
+++ A_Divisibility_Problem.cpp
@@ -10,7 +10,22 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
 
-    
+    int t;
+    long long int a, b;
+    cin >> t;
+    while (t--)
+    {
+        int div = 0, pls = 0;
+        cin >> a >> b;
+        if (a % b == 0)
+        {
+            cout << 0 << endl;
+            continue;
+        }
+        div = a / b;
+        pls = (div + 1) * b;
+        cout << pls - a << endl;
+    }
 
     return 0;
 }
