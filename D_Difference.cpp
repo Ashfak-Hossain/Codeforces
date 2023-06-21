9bbc2eb5c2de9c1239f4fa9f3371d879b983ad03
Berlin <evan1234.ek@gmail.com>
2023-06-21 21:29:37 +0600

    Solved

---
+++ D_Difference.cpp
@@ -0,0 +1,20 @@
+/**
+ *        Author:  Ashfak Hossain Evan, American International University, Bangladesh
+ *        Created: 21/06/2023 21:27:03
+ **/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    long long a, b, c, d;
+
+    cin >> a >> b >> c >> d;
+
+    long long x = (a * b) - (c * d);
+
+    cout << "Difference = " << x << endl;
+
+    return 0;
+}
---
+++ E_Area_of_a_Circle.cpp
@@ -0,0 +1,13 @@
+/**
+ *        Author:  Ashfak Hossain Evan, American International University, Bangladesh
+ *        Created: 21/06/2023 21:29:32
+ **/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
