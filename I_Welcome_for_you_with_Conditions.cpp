185075a41da56c10b366f83b6e25dad8bcad654c
Berlin <evan1234.ek@gmail.com>
2023-06-21 22:06:16 +0600

    Solved

---
+++ I_Welcome_for_you_with_Conditions.cpp
@@ -0,0 +1,18 @@
+/**
+ *        Author:  Ashfak Hossain Evan, American International University, Bangladesh
+ *        Created: 21/06/2023 22:00:48
+ **/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cin >> a >> b;
+
+    cout << (a >= b ? "Yes" : "No") << endl;
+
+    return 0;
+}
