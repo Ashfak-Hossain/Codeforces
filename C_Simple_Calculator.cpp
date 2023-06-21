79e7dd28020ec814c31a09068e3473f811b8d758
Berlin <evan1234.ek@gmail.com>
2023-06-21 21:26:38 +0600

    Solved

---
+++ C_Simple_Calculator.cpp
@@ -0,0 +1,19 @@
+/**
+ *        Author:  Ashfak Hossain Evan, American International University, Bangladesh
+ *        Created: 21/06/2023 21:22:38
+ **/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    long long n, m;
+    cin >> n >> m;
+
+    cout << n << " + " << m << " = " << n + m << endl;
+    cout << n << " * " << m << " = " << n * m << endl;
+    cout << n << " - " << m << " = " << n - m << endl;
+
+    return 0;
+}
