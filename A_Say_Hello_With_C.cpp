3725796a51d4268b929a9a563d61982f63308f99
Berlin <evan1234.ek@gmail.com>
2023-06-21 21:22:27 +0600

    Solved

---
+++ A_Say_Hello_With_C.cpp
@@ -0,0 +1,17 @@
+/**
+ *        Author:  Ashfak Hossain Evan, American International University, Bangladesh
+ *        Created: 21/06/2023 21:16:00
+ **/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    cout << "Hello, " << s << endl;
+
+    return 0;
+}
---
+++ B_Basic_Data_Types.cpp
@@ -0,0 +1,25 @@
+/**
+ *        Author:  Ashfak Hossain Evan, American International University, Bangladesh
+ *        Created: 21/06/2023 21:20:13
+ **/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    long long l;
+    char c;
+    float f;
+    double d;
+
+    cin >> i >> l >> c >> f >> d;
+    cout << i << endl;
+    cout << l << endl;
+    cout << c << endl;
+    cout << f << endl;
+    cout << d << endl;
+
+    return 0;
+}
