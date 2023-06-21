6216851ce01066be07ee4000a3e5ec6c5f3f6f71
Berlin <evan1234.ek@gmail.com>
2023-06-21 21:49:40 +0600

    Solved

---
+++ G_Summation_from_1_to_N.cpp
@@ -0,0 +1,18 @@
+/**
+ *        Author:  Ashfak Hossain Evan, American International University, Bangladesh
+ *        Created: 21/06/2023 21:40:23
+ **/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    long long n;
+
+    cin >> n;
+
+    cout << (n * (n + 1)) / 2 << endl;
+
+    return 0;
+}
