5055a104ca808058133a2dda19a71389cbb18365
Berlin <evan1234.ek@gmail.com>
2023-06-21 21:40:05 +0600

    Solved

---
+++ F_Digits_Summation.cpp
@@ -0,0 +1,18 @@
+/**
+ *        Author:  Ashfak Hossain Evan, American International University, Bangladesh
+ *        Created: 21/06/2023 21:34:31
+ **/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    long long n, m;
+
+    cin >> n >> m;
+
+    cout << (n % 10) + (m % 10) << endl;
+
+    return 0;
+}
