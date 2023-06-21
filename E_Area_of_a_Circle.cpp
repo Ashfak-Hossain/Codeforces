fddab053b70214e593fe8d91647316a709fe30a6
Berlin <evan1234.ek@gmail.com>
2023-06-21 21:34:06 +0600

    Solved

--- E_Area_of_a_Circle.cpp
+++ E_Area_of_a_Circle.cpp
@@ -8,6 +8,15 @@ using namespace std;
 
 int main()
 {
+    long double r;
+
+    long double pi = 3.141592653;
+
+    cin >> r;
+
+    long double area = pi * pow(r, 2);
+
+    cout << fixed << setprecision(9) << area << endl;
 
     return 0;
 }
