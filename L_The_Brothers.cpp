03eed5123c2544c7564f64a71861fdbcedb770a1
Berlin <evan1234.ek@gmail.com>
2023-06-22 21:48:11 +0600

    Solved

---
+++ L_The_Brothers.cpp
@@ -0,0 +1,27 @@
+/**
+ *        Author:  Ashfak Hossain Evan, American International University, Bangladesh
+ *        Created: 22/06/2023 21:41:24
+ **/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string s1, s2, s3, s4;
+
+    cin >> s1 >> s2;
+
+    cin >> s3 >> s4;
+
+    if (s2 == s4)
+    {
+        cout << "ARE Brothers" << endl;
+    }
+    else
+    {
+        cout << "NOT" << endl;
+    }
+
+    return 0;
+}
