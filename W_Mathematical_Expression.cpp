4e60750cafa9c15b7fc4621cec2cefaee1eb426a
Berlin <evan1234.ek@gmail.com>
2023-06-25 15:56:13 +0600

    Solved

---
+++ W_Mathematical_Expression.cpp
@@ -0,0 +1,46 @@
+/**
+ *        Author:  Ashfak Hossain Evan, American International University, Bangladesh
+ *        Created: 25/06/2023 15:50:37
+ **/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+
+    char x, y;
+
+    cin >> a >> x >> b >> y >> c;
+
+    if (x == '+' && (a + b) == c)
+    {
+        cout << "Yes" << endl;
+    }
+    else if (x == '-' && (a - b) == c)
+    {
+        cout << "Yes" << endl;
+    }
+    else if (x == '*' && (a * b) == c)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        if (x == '+')
+        {
+            cout << a + b << endl;
+        }
+        else if (x == '-')
+        {
+            cout << a - b << endl;
+        }
+        else if (x == '*')
+        {
+            cout << a * b << endl;
+        }
+    }
+
+    return 0;
+}
