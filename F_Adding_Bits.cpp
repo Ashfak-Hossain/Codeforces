0060c3f7d263ab053fad1b0c6343eed73b455dd2
Berlin <evan1234.ek@gmail.com>
2023-06-28 19:58:22 +0600

    solved

---
+++ F_Adding_Bits.cpp
@@ -0,0 +1,18 @@
+/**
+ *        Author:  Ashfak Hossain Evan, American International University, Bangladesh
+ *        Created: 28/06/2023 19:14:36
+ **/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    long long num1, num2;
+
+    cin >> num1 >> num2;
+
+    cout << (num1 ^ num2) << endl;
+
+    return 0;
+}
