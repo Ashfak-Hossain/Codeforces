74cd0b0819c932c3e3a1d7839080ef8b05cc6e54
Berlin <evan1234.ek@gmail.com>
2023-06-29 18:40:02 +0600

    Solved

---
+++ A_1_to_N.cpp
@@ -0,0 +1,21 @@
+/**
+ *        Author:  Ashfak Hossain Evan, American International University, Bangladesh
+ *        Created: 29/06/2023 18:39:01
+ **/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    for (size_t i = 1; i <= n; i++)
+    {
+        cout << i << endl;
+    }
+
+    return 0;
+}
