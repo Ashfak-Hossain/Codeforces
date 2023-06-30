6db5f14adcd97178dc7849872a2ee9a3fc9d1be6
Berlin <evan1234.ek@gmail.com>
2023-06-30 14:57:11 +0600

    solved

---
+++ D_Fixed_Password.cpp
@@ -0,0 +1,27 @@
+/**
+ *        Author:  Ashfak Hossain Evan, American International University, Bangladesh
+ *        Created: 30/06/2023 14:56:32
+ **/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int x;
+
+    while (cin >> x)
+    {
+        if (x != 1999)
+        {
+            cout << "Wrong" << endl;
+        }
+        else
+        {
+            cout << "Correct" << endl;
+            break;
+        }
+    }
+
+    return 0;
+}
