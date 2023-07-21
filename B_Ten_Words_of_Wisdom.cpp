2b82f4c88a3153f04e49329e0e7b1e9259d06564
Berlin <evan1234.ek@gmail.com>
2023-07-21 21:40:34 +0600

    Solved

---
+++ B_Ten_Words_of_Wisdom.cpp
@@ -0,0 +1,89 @@
+/**
+ *        Bismillahir Rahmanir Rahim
+ *        Author:  Ashfak Hossain Evan, American International University, Bangladesh
+ *        Created: 21/07/2023 21:09:38
+ **/
+#include <bits/stdc++.h>
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) int(x.size())
+#define char2Int(c) (c - '0')
+#define lastEle(vec) vec[vec.size() - 1]
+#define PI 3.1415926535897932384626433832795l
+#define endl '\n'
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<vi> vvi;
+typedef map<int, int> mpii;
+typedef set<int> seti;
+typedef multiset<int> mseti;
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+struct res
+{
+    int words;
+    int quality;
+};
+
+void solve()
+{
+    int n;
+
+    cin >> n;
+
+    vector<res> r(n);
+
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> r[i].words >> r[i].quality;
+    }
+
+    int maxq = 0;
+    int winnerIndex = -1;
+
+    for (int i = 0; i < n; ++i)
+    {
+        if (r[i].words <= 10 && r[i].quality > maxq)
+        {
+            maxq = r[i].quality;
+            winnerIndex = i;
+        }
+    }
+
+    cout << winnerIndex + 1 << endl;
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("error.txt", "w", stderr);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int _;
+    cin >> _;
+    while (_--)
+        solve();
+
+    return 0;
+}
