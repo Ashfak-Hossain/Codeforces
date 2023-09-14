a2cf0b8cc2764d72388b04c98e2f58ecd3ad4886
Berlin <evan1234.ek@gmail.com>
2023-09-14 17:35:51 +0600

    solved

---
+++ A_Construct_a_Rectangle.cpp
@@ -0,0 +1,84 @@
+/**
+ *        Bismillahir Rahmanir Rahim
+ *        Author:  Ashfak Hossain Evan, American International University, Bangladesh
+ *        Created: 14/09/2023 17:35:35
+ **/
+#include <bits/stdc++.h>
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) int(x.size())
+#define char2Int(c) (c - '0')
+#define lastEle(vec) vec[vec.size() - 1]
+#define PI 3.1415926535897932384626433832795l
+#define endl '\n'
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<vi> vvi;
+typedef map<int, int> mpii;
+typedef set<int> seti;
+typedef multiset<int> mseti;
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve()
+{
+
+    vi v(3);
+
+    for (int i = 0; i < 3; ++i)
+    {
+        cin >> v[i];
+    }
+
+    bool ok = false;
+
+    for (int i = 0; i < 3; ++i)
+    {
+        ok |= v[i] == v[(i + 1) % 3] + v[(i + 2) % 3];
+    }
+
+    for (int i = 0; i < 3; ++i)
+    {
+        if (v[i] % 2 == 0)
+        {
+            ok |= v[(i + 1) % 3] == v[(i + 2) % 3];
+        }
+    }
+
+    cout << (ok ? "YES" : "NO") << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("error.txt", "w", stderr);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int _;
+    cin >> _;
+    while (_--)
+        solve();
+
+    return 0;
+}
