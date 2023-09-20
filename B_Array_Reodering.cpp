a92e87b92246dab82129ef12845986a8a0b267a2
Berlin <evan1234.ek@gmail.com>
2023-09-20 12:58:29 +0600

    solved

---
+++ B_Array_Reodering.cpp
@@ -0,0 +1,85 @@
+/**
+ *        Bismillahir Rahmanir Rahim
+ *        Author:  Ashfak Hossain Evan, American International University, Bangladesh
+ *        Created: 20/09/2023 12:58:14
+ **/
+#include <bits/stdc++.h>
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) int(x.size())
+#define char2Int(c) (c - '0')
+#define lastEle(vec) vec[vec.size() - 1]
+#define PI 3.1415926535897932384626433832795l
+#define endl '\n'
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<vi> vvi;
+typedef map<int, int> mpii;
+typedef set<int> seti;
+typedef multiset<int> mseti;
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve()
+{
+
+    int n;
+    cin >> n;
+
+    vi v(n);
+
+    for (auto &i : v)
+    {
+        cin >> i;
+    }
+
+    sort(v.begin(), v.end(), [](int x, int y)
+         { return x % 2 < y % 2; });
+
+    int ans(0);
+
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = i + 1; j < n; ++j)
+        {
+            ans += gcd(v[i], v[j] * 2) > 1;
+        }
+    }
+
+    cout << ans << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("error.txt", "w", stderr);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int _;
+    cin >> _;
+    while (_--)
+        solve();
+
+    return 0;
+}
