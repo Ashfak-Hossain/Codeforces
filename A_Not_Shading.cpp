4c1cf8ed6ef1c27491519d1caef59c21d835e51b
Berlin <evan1234.ek@gmail.com>
2023-09-20 11:39:09 +0600

    solved

---
+++ A_Not_Shading.cpp
@@ -0,0 +1,102 @@
+/**
+ *        Bismillahir Rahmanir Rahim
+ *        Author:  Ashfak Hossain Evan, American International University, Bangladesh
+ *        Created: 20/09/2023 11:39:00
+ **/
+#include <bits/stdc++.h>
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) int(x.size())
+#define char2Int(c) (c - '0')
+#define lastEle(vec) vec[vec.size() - 1]
+#define PI 3.1415926535897932384626433832795l
+#define endl '\n'
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<vi> vvi;
+typedef map<int, int> mpii;
+typedef set<int> seti;
+typedef multiset<int> mseti;
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve()
+{
+
+    int n, m, r, c;
+    cin >> n >> m >> r >> c;
+    r--;
+    c--;
+    char a[n + 7][m + 7];
+    bool hasB = false;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> a[i][j];
+            hasB |= (a[i][j] == 'B');
+        }
+    }
+    if (!hasB)
+    {
+        cout << -1 << '\n';
+        return;
+    }
+    if (a[r][c] == 'B')
+    {
+        cout << 0 << '\n';
+        return;
+    }
+    bool oneMove = false;
+    for (int i = 0; i < n; i++)
+    {
+        oneMove |= (a[i][c] == 'B');
+    }
+    for (int j = 0; j < m; j++)
+    {
+        oneMove |= (a[r][j] == 'B');
+    }
+    if (oneMove)
+    {
+        cout << 1 << '\n';
+    }
+    else
+    {
+        cout << 2 << '\n';
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("error.txt", "w", stderr);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int _;
+    cin >> _;
+    while (_--)
+        solve();
+
+    return 0;
+}
