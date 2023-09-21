62378e256bdca71f544d9a3f60814d3b8484f391
Berlin <evan1234.ek@gmail.com>
2023-09-21 22:38:32 +0600

    solved

---
+++ B_Good_Kid.cpp
@@ -0,0 +1,97 @@
+/**
+ *        Bismillahir Rahmanir Rahim
+ *        Author:  Ashfak Hossain Evan, American International University, Bangladesh
+ *        Created: 21/09/2023 22:38:16
+ **/
+#include <bits/stdc++.h>
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) int(x.size())
+#define char2Int(c) (c - '0')
+#define lastEle(vec) vec[vec.size() - 1]
+#define PI 3.1415926535897932384626433832795l
+#define endl '\n'
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<vi> vvi;
+typedef map<int, int> mpii;
+typedef set<int> seti;
+typedef multiset<int> mseti;
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve()
+{
+
+    ll n;
+
+    cin >> n;
+
+    vector<ll> a(n);
+
+    ll smallest_non_zero = 10;
+
+    for (ll i = 0; i < n; ++i)
+    {
+        cin >> a[i];
+        if (a[i] < smallest_non_zero)
+        {
+            smallest_non_zero = a[i];
+        }
+    }
+
+    debug(a);
+
+    for (ll i = 0; i < n; ++i)
+    {
+        if (a[i] == smallest_non_zero)
+        {
+            a[i]++;
+            break;
+        }
+    }
+
+    ll product = 1;
+
+    for (ll i = 0; i < n; ++i)
+    {
+        product *= a[i];
+    }
+
+    cout << product << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("error.txt", "w", stderr);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int _;
+    cin >> _;
+    while (_--)
+        solve();
+
+    return 0;
+}
