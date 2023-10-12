f6564a51305d65fa79da233c483aa9f4002ed5ff
Berlin <evan1234.ek@gmail.com>
2023-10-12 20:49:07 +0600

    solved

---
+++ A_Don_t_Try_to_Count.cpp
@@ -0,0 +1,81 @@
+/**
+ *        Bismillahir Rahmanir Rahim
+ *        Author:  Ashfak Hossain Evan, American International University, Bangladesh
+ **/
+#include <bits/stdc++.h>
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) int(x.size())
+#define char2Int(c) (c - '0')
+#define lastEle(vec) vec[vec.size() - 1]
+#define PI 3.1415926535897932384626433832795l
+#define endl '\n'
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<vi> vvi;
+typedef map<int, int> mpii;
+typedef set<int> seti;
+typedef multiset<int> mseti;
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    string x, s;
+    cin >> x >> s;
+
+    int cnt = 0;
+    while (true)
+    {
+        if (x.find(s) != string::npos)
+        {
+            cout << cnt << endl;
+            break;
+        }
+        x += x;
+        cnt++;
+
+        if (x.length() > 100)
+        {
+            cout << -1 << endl;
+            break;
+        }
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("error.txt", "w", stderr);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int _;
+    cin >> _;
+    while (_--)
+        solve();
+
+    return 0;
+}
