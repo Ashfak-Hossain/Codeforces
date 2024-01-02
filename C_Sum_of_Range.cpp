98013fad0ecaa3725030cb8b519ca6dc098748e0
Berlin <evan1234.ek@gmail.com>
2024-01-02 23:53:43 +0600

    solved

---
+++ C_Sum_of_Range.cpp
@@ -0,0 +1,108 @@
+/**
+ *        Bismillahir Rahmanir Rahim
+ *        Author:  Ashfak Hossain Evan, American International University, Bangladesh
+ **/
+#include <bits/stdc++.h>
+
+#ifdef BERLIN
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((long long)x.size())
+#define char2Int(c) (c - '0')
+#define lastEle(vec) vec[vec.size() - 1]
+#define PI 3.1415926535897932384626433832795l
+#define endl '\n'
+#define f first
+#define s second
+#define pb push_back
+#define range(i, v) for (auto &i : v)
+#define fix(prec)                            \
+    {                                        \
+        cout << setprecision(prec) << fixed; \
+    }
+#define mem(arr, v) memset(arr, v, sizeof(arr))
+#define getunique(v)                      \
+    {                                     \
+        sort(all(v));                     \
+        v.erase(unique(all(v)), v.end()); \
+    }
+#define Muichiro ios_base::sync_with_stdio(false);
+#define Tokito cin.tie(NULL);
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+long long sum_odd(long long num)
+{
+    long long result = (num + 1) / 2;
+    return result * result;
+}
+long long sum_even(long long num)
+{
+    num /= 2;
+    long long result = num * (num + 1);
+    return result;
+}
+
+long long sum(long long num)
+{
+    long long result = (num * (num + 1)) / 2;
+    return result;
+}
+
+void Free_Palestine()
+{
+
+    long long num1, num2;
+
+    cin >> num1 >> num2;
+
+    long long bigger_num = max(num1, num2);
+    long long smaller_num = min(num1, num2);
+
+    long long sum_all = sum(bigger_num) - sum(smaller_num - 1);
+    long long odd_sum = sum_odd(bigger_num) - sum_odd(smaller_num - 1);
+    long long even_sum = sum_even(bigger_num) - sum_even(smaller_num - 1);
+
+    cout << sum_all << endl;
+    cout << even_sum << endl;
+    cout << odd_sum << endl;
+}
+
+int main()
+{
+    Muichiro Tokito
+
+#ifdef BERLIN
+        clock_t tStart = clock();
+    freopen("input.txt", "r", stdin);
+    freopen("error.txt", "w", stderr);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    Free_Palestine();
+
+#ifdef BERLIN
+    fprintf(stderr, "\nRuntime: %.10fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
+#endif
+
+    return 0;
+}
