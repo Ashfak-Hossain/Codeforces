2fbd1e90e162657e4148fbdb363e37abf969a12e
Berlin <evan1234.ek@gmail.com>
2024-01-09 10:53:56 +0600

    solved

---
+++ V_Another_Sorting_Problem.cpp
@@ -0,0 +1,115 @@
+/**
+ *        Bismillahir Rahmanir Rahim
+ *        Author:  Ashfak Hossain Evan, American International University, Bangladesh
+ **/
+#include <bits/stdc++.h>
+
+#ifdef BERLIN
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((long long)x.size())
+#define char2Int(c) (c - '0')
+#define lastEle(vec) vec[vec.size() - 1]
+#define PI 3.1415926535897932384626433832795l
+#define endl '\n'
+#define f first
+#define s second
+#define pb push_back
+#define range(i, v) for (auto &i : v)
+#define fix(prec)                            \
+    {                                        \
+        cout << setprecision(prec) << fixed; \
+    }
+#define mem(arr, v) memset(arr, v, sizeof(arr))
+#define getunique(v)                      \
+    {                                     \
+        sort(all(v));                     \
+        v.erase(unique(all(v)), v.end()); \
+    }
+#define Muichiro ios_base::sync_with_stdio(false);
+#define Tokito cin.tie(NULL);
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void Free_Palestine()
+{
+    int n, m;
+    cin >> n >> m;
+
+    vector<pair<string, int>> s(n);
+
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> s[i].f;
+        s[i].s = i + 1;
+    }
+
+    sort(all(s), [=](const pair<string, int> &a, const pair<string, int> &b)
+         {
+        for (int i = 0; i < m; ++i)
+        {
+            if (i & 1)
+            {
+                if (a.f[i] != b.f[i])
+                {
+                    if (a.f[i] > b.f[i])
+                        return true;
+                    else
+                        return false;
+                }
+            }else{
+                if (a.f[i] != b.f[i])
+                {
+                    if (a.f[i] < b.f[i])
+                        return true;
+                    else
+                        return false;
+                }
+            }
+        }
+        return false; });
+
+    for (int i = 0; i < n; ++i)
+        cout << s[i].s << " ";
+
+    cout << endl;
+}
+
+int main()
+{
+    Muichiro Tokito
+
+#ifdef BERLIN
+        clock_t tStart = clock();
+    freopen("input.txt", "r", stdin);
+    freopen("error.txt", "w", stderr);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    Free_Palestine();
+
+#ifdef BERLIN
+    fprintf(stderr, "\nRuntime: %.10fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
+#endif
+
+    return 0;
+}
